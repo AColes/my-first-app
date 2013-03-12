1a4f90683e09725de7108dcde2e81053c658d1f5
Adrienne Coles <Adrienne@kuroiweb.com>
2013-03-12 18:50:38 +0000

    notification (pop-up) should show in native style (browser/app)

--- js/main.js
+++ js/main.js
@@ -13,13 +13,21 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function() {
-        // this.store = new MemoryStore();
-        this.store = new LocalStorageStore();
-        //this.store = new WebSqlStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('(Memory) Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-
 };
 
 app.initialize();
