f22287df422c20ccf88834f5721bc0926d7bb56e
Adrienne Coles <Adrienne@kuroiweb.com>
2013-03-13 13:42:06 +0000

    using handlebar templates

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 var app = {
 
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     findByName: function() {
-        console.log('findByName');
+    var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
+    },
+
+    initialize: function() {
+        var self            = this;
+        this.store          = new MemoryStore(function() {
+            self.renderHomeView();
+        });        
+        this.homeTpl        = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl  = Handlebars.compile($("#employee-li-tpl").html());        
     },
 
     showAlert: function (message, title) {
@@ -21,13 +29,6 @@ var app = {
         }
     },
 
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('(Memory) Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
 };
 
 app.initialize();
